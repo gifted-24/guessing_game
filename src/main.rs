ace9139f203fb7e8e1f9ac72468a9c2b356cd7ea
gift esefo <giftesefo78@gmail.com>
2025-08-20 09:24:49 +0000

    update to Cargo.toml file
    - updated `rand 0.8.5` -> `rand 0.9.2`.
    - refactored `main.rs` to reflect change to avoid `Exceptions` in `compile time`

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,9 @@ use std::cmp::Ordering;
 fn input() -> String {
     let mut buf = String::new();
     io::stdin().read_line(&mut buf).expect("Failed to read line");
-    buf
-}
 
 fn main() {
-    let secret_num: u8 = rand::thread_rng().gen_range(1..=100);
+    let secret_num: u8 = rand::rng().random_range(1..=100);
     loop {
         println!("Enter your guess: ");
         let num: u8 = match input().trim().parse() {
